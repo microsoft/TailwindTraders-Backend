bd28bf7ec8146fe3eb6d122cbabb320cd3a0cf52
eiximenis <etomas@gmail.com>
2018-12-21 09:07:09 +0100

    addinig missing file

---
+++ Source/Services/Tailwind.Traders.Cart.Api/models/recommendedDao.js
@@ -0,0 +1,50 @@
+class RecommendedDao {
+    constructor(cosmosClient, databaseId) {
+        this.client = cosmosClient;
+        this.databaseId = databaseId;
+        this.collectionId = 'recommendations';
+
+        this.database = null;
+        this.container = null;
+    }
+
+    async init() {
+        const dbResponse = await this.client.databases.createIfNotExists({
+            id: this.databaseId
+        });
+        this.database = dbResponse.database;
+        const coResponse = await this.database.containers.createIfNotExists({
+            id: this.collectionId
+        });
+        this.container = coResponse.container;
+    }
+
+
+    async findRelated(typeid, email) {
+        let typeidToNumber = parseInt(typeid)
+        const querySpec = {
+            query: "SELECT * FROM r WHERE r.email=@email and r.typeid=@typeid",
+            parameters: [
+                {
+                    name: "@email",
+                    value: email,
+                },
+                {
+                    name: "@typeid",
+                    value: typeidToNumber,
+                }
+            ]
+        };
+        if (!this.container) {
+            throw new Error("Collection is not initialized.");
+        }
+        const { result: results } = await this.container.items.query(querySpec).toArray();
+        return results.map(i => ({
+            email: i.email,
+            typeid: i.typeid,
+            recommendations: i.recommendations
+        }));
+    }
+}
+
+module.exports = RecommendedDao;
