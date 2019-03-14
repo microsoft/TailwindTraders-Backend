f8a66cb351fc14063bdc8b43b90577b36a023f37
= <etorre@plainconcepts.com>
2019-03-14 12:28:52 +0100

    add endpoint and docker config

---
+++ Source/ApiGWs/Tailwind.Traders.Bff/Models/TokenRequestModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Tailwind.Traders.MobileBff.Models
+{
+    using Newtonsoft.Json;
+
+    public class TokenRequestModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        [JsonProperty(PropertyName = "grant_type")]
+        public string GrantType { get; set; }
+    }
+}
