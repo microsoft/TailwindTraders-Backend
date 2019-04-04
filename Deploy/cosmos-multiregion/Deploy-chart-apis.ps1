84c11e19fbca920c42b05b23dc3606cde96ce75c
eiximenis <etomas@gmail.com>
2019-04-04 18:01:09 +0200

    deploy cosmosdb multiregion scripts

---
+++ Deploy/cosmos-multiregion/Deploy-chart-apis.ps1
@@ -0,0 +1,35 @@
+Param(
+    [parameter(Mandatory=$true)][string]$resourceGroup,
+    [parameter(Mandatory=$true)][string]$region1,
+    [parameter(Mandatory=$true)][string]$region2,
+    [parameter(Mandatory=$true)][string]$acrName,
+    [parameter(Mandatory=$true)][string]$webappNamePrefix,
+    [parameter(Mandatory=$true)][string]$cartCosmosDbName,
+    [parameter(Mandatory=$false)][string]$servicePlanSku="S1",
+    [parameter(Mandatory=$false)][string]$imageTag="latest"
+)
+
+$acrLoginServer=$(az acr show -g $resourceGroup -n $acrName | ConvertFrom-Json).loginServer
+$acrCredentials=$(az acr credential show -g $resourceGroup -n $acrName | ConvertFrom-Json)
+$cosmosCredentials=$(az cosmosdb list-keys -g $resourceGroup -n $cartCosmosDbName | ConvertFrom-Json).primaryMasterKey
+$cosmos=$(az cosmosdb show -g $resourceGroup -n $cartCosmosDbName | ConvertFrom-Json)
+$cosmosEndpoint=$cosmos.documentEndpoint
+
+Write-Host "Creating service plan for chart api 1" -ForegroundColor Yellow
+az appservice plan create --name ${webappNamePrefix}-splan1 --resourceGroup $resourceGroup --sku $servicePlanSku --is-linux
+
+Write-Host "Creating service plan for chart api 1" -ForegroundColor Yellow
+az appservice plan create --name ${webappNamePrefix}-splan2 --resourceGroup $resourceGroup --sku $servicePlanSku --is-linux
+
+Write-Host "Creating webapp for chart api1"
+az webapp plan create --name "$webappNamePrefix-1" --plan ${webappNamePrefix}-splan1 --resourceGroup $resourceGroup -deployment-container-image-name nginx
+az webapp config container set -g $resourceGroup -n "$webappNamePrefix-1" --docker-registry-server-url https://$acrLoginServer --docker-custom-image-name $acrLoginServer/cart.api:${imageTag}
+az webapp config appsettings set -g $resourceGroup -n "$webappNamePrefix-1" --settings AUTHKEY=$cosmosCredentials HOST=$cosmosEndpoint ISSUER=TTFakeLogin SECURITYKEY=nEpLzQJGNSCNL5H6DIQCtTdNxf5VgAGcBbtXLms1YDD01KJBAs0WVawaEjn97uwB
+
+
+Write-Host "Creating webapp for chart api2"
+az webapp plan create --name "$webappNamePrefix-2" --plan ${webappNamePrefix}-splan2 --resourceGroup $resourceGroup -deployment-container-image-name nginx
+az webapp config container set -g $resourceGroup -n "$webappNamePrefix-2" --docker-registry-server-url https://$acrLoginServer --docker-custom-image-name $acrLoginServer/cart.api:${imageTag}
+
+
+
---
+++ Deploy/cosmos-multiregion/Deploy-webs.ps1
@@ -0,0 +1,41 @@
+Param(
+    [parameter(Mandatory=$true)][string]$resourceGroup,
+    [parameter(Mandatory=$true)][string]$region1,
+    [parameter(Mandatory=$true)][string]$region2,
+    [parameter(Mandatory=$true)][string]$acrName,
+    [parameter(Mandatory=$true)][string]$apiNamePrefix,
+    [parameter(Mandatory=$true)][string]$webappNamePrefix,
+    [parameter(Mandatory=$true)][string]$apiurl,
+    [parameter(Mandatory=$false)][string]$servicePlanSku="S1",
+    [parameter(Mandatory=$false)][string]$imageTag="latest"
+)
+
+$acrLoginServer=$(az acr show -g $resourceGroup -n $acrName | ConvertFrom-Json).loginServer
+$acrCredentials=$(az acr credential show -g $resourceGroup -n $acrName | ConvertFrom-Json)
+
+Write-Host "Creating service plan for web 1 in $region1" -ForegroundColor Yellow
+#az appservice plan create --name ${webappNamePrefix}-splan1 -g $resourceGroup --sku $servicePlanSku --location $region1 --is-linux
+
+Write-Host "Creating service plan for web 2 in $region2" -ForegroundColor Yellow
+#az appservice plan create --name ${webappNamePrefix}-splan2 -g $resourceGroup --sku $servicePlanSku --location $region2 --is-linux
+
+Write-Host "Creating webapp for web1 in $region1"
+#az webapp  create  --name "$webappNamePrefix-$region1" --plan ${webappNamePrefix}-splan1 -g $resourceGroup --deployment-container-image-name nginx
+#az webapp config container set -g $resourceGroup -n "$webappNamePrefix-$region1" --docker-registry-server-url https://$acrLoginServer --docker-custom-image-name $acrLoginServer/web:${imageTag}
+
+Write-Host "Creating webapp for web2 in $region2"
+#az webapp  create --name "$webappNamePrefix-$region2" --plan ${webappNamePrefix}-splan2 -g $resourceGroup --deployment-container-image-name nginx
+#az webapp config container set -g $resourceGroup -n "$webappNamePrefix-$region2" --docker-registry-server-url https://$acrLoginServer --docker-custom-image-name $acrLoginServer/web:${imageTag}
+
+$api1=$(az webapp show  -g $resourceGroup --name "$apiNamePrefix-$region1" | ConvertFrom-Json)
+$api2=$(az webapp show -g $resourceGroup --name "$apiNamePrefix-$region2" | ConvertFrom-Json)
+
+$api1HostName=$api1.hostNames[0]
+$api2HostName=$api2.hostNames[0]
+
+ 
+Write-Host "Setting configuration of web1 to use $api1HostName for cart API" -ForegroundColor Yellow
+az webapp config appsettings set -g $resourceGroup -n "$webappNamePrefix-$region1" --settings ApiUrlShoppingCart="https://$api1HostName" ApiUrl=$apiurl
+
+Write-Host "Setting configuration of web2 to use $api2HostName for cart API" -ForegroundColor Yellow
+az webapp config appsettings set -g $resourceGroup -n "$webappNamePrefix-$region2" --settings ApiUrlShoppingCart="https://$api2HostName" ApiUrl=$apiurl
