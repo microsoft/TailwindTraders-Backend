cc14dd210c3daaab9e52ef2115bc0fdb81a29b60
OriolBonjoch <oriol.bonjoch@gmail.com>
2019-07-02 14:50:38 +0200

    Feature/create wcf service (#3)
    * Add scaffolding WCF
    
    * Copied Customer logic from TT Rewards
    
    * Add DockerFile
    
    * Fix csproj semantic error
    
    * Added checks for empty customer

---
+++ Source/Services/Tailwind.Traders.Rewards.Registration.Api/UserService.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Tailwind.Traders.Rewards.Registration.Api.Repositories;
+
+namespace Tailwind.Traders.Rewards.Registration.Api
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class UserService : IUserService
+    {
+        private readonly CustomerRepository _customerRepository;
+
+        public UserService()
+        {
+            _customerRepository = new CustomerRepository();
+        }
+
+        public bool Registration(string email)
+        {
+            try
+            {
+                var user = _customerRepository.GetCustomerByEmailOrName(email);
+                if (user != null)
+                {
+                    return false;
+                }
+
+                _customerRepository.InsertCustomer(email);
+
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
