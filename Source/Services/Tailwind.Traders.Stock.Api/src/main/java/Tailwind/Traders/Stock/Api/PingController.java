02c501197861060f7c849d41d4f5b931e91d13ea
Ramón Tomás <rtomas@plainconcepts.com>
2019-07-23 16:39:53 +0200

    Added heatlhcheck in k8s for Stock api

--- Source/Services/Tailwind.Traders.Stock.Api/src/main/java/Tailwind/Traders/Stock/Api/PingController.java
+++ Source/Services/Tailwind.Traders.Stock.Api/src/main/java/Tailwind/Traders/Stock/Api/PingController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
 import springfox.documentation.annotations.ApiIgnore;
 
 @Controller
