dd99489112f3aced195460ed4d421ed55e9f784c
Ramón Tomás <rtomas@plainconcepts.com>
2019-07-24 13:27:40 +0200

    Adding health check to Popular products api and updated helms scripts to use it in AKS

--- Source/Services/Tailwind.Traders.PopularProduct.Api/middlewares/registerGlobalMiddlewares.go
+++ Source/Services/Tailwind.Traders.PopularProduct.Api/middlewares/registerGlobalMiddlewares.go
@@ -2,12 +2,14 @@ package middlewares
 
 import (
 	"net/http"
+
 	"github.com/rs/cors"
 )
 
 func SetupGlobalMiddlewares(handler http.Handler) http.Handler {
 	corsHandler := cors.Default().Handler
+	healthCheckHandler := healthCheckMiddleware
 	handler = corsHandler(handler)
-
-	return handler;
+	handler = healthCheckHandler(handler)
+	return handler
 }
