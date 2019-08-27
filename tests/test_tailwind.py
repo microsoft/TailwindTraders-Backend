8b933703d4bf077c16d57d5c89bf8e998e250cc0
Neil Peterson <neilpeterson@outlook.com>
2019-08-27 16:41:46 -0700

    MOC tests

---
+++ tests/test_tailwind.py
@@ -0,0 +1,16 @@
+import pytest
+import math
+
+def test_cart():
+   num = 25
+   assert math.sqrt(num) == 5
+
+def test_products():
+   num = 25
+   assert math.sqrt(num) == 5
+
+def test_helm():
+   assert 2*20 == 40
+
+def test_login():
+   assert 2*20 == 40
