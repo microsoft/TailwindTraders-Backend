69bf474c10c8309c23c9f3e34ffd2ed57f2c1051
eiximenis <etomas@gmail.com>
2019-08-29 18:13:17 +0200

    Minor script updates

--- Deploy/Create-WinLinux-Aks.ps1
+++ Deploy/Create-WinLinux-Aks.ps1
@@ -4,10 +4,17 @@ Param(
     [parameter(Mandatory=$true)][string]$clientId,
     [parameter(Mandatory=$true)][string]$password,
     [parameter(Mandatory=$true)][string]$winUser,
-    [parameter(Mandatory=$true)][string]$winPassword
+    [parameter(Mandatory=$true)][string]$winPassword,
+    [parameter(Mandatory=$false)][string]$aksName,
+    [parameter(Mandatory=$false)][int]$linuxNodes=3,
+    [parameter(Mandatory=$false)][int]$winNodes=2
 )
 
 $rg = $(az group show -n $resourceGroup -o json | ConvertFrom-Json)
+
+if (-not $rg) {
+    $rg=$(az group create -n $resourceGroup -l $location)
+}
 
 Write-Host "Install required az-cli extension aks-preview" -ForegroundColor Yellow
 az extension add --name aks-preview
@@ -16,16 +23,20 @@ Write-Host "Getting last AKS version in location $location" -ForegroundColor Yel
 $aksVersions=$(az aks get-versions -l $location --query  orchestrators[].orchestratorVersion -o json | ConvertFrom-Json)
 $aksLastVersion=$aksVersions[$aksVersions.Length-1]
 
- Write-Host "AKS last version is $aksLastVersion" -ForegroundColor Yellow
+Write-Host "AKS last version is $aksLastVersion" -ForegroundColor Yellow
 if (-not $aksLastVersion.StartsWith("1.14")) {
     Write-Host "AKS 1.14 required. Exiting." -ForegroundColor Red
     exit 1
 }
 
+if ([String]::IsNullOrEmpty($aksName)) {
+    $aksName=$resourceGroup
+}
+
 Write-Host "Creating AKS..." -ForegroundColor Yellow
 az aks create --resource-group $resourceGroup `
-    --name $resourceGroup `
-    --node-count 2 `
+    --name $aksName `
+    --node-count $linuxNodes `
     --enable-addons monitoring `
     --generate-ssh-keys `
     --kubernetes-version $aksLastVersion   `
@@ -38,8 +49,8 @@ az aks create --resource-group $resourceGroup `
 
 Write-Host "Configuring AKS nodes..." -ForegroundColor Yellow
 az aks nodepool add --resource-group $resourceGroup `
-    --cluster-name $resourceGroup `
+    --cluster-name $aksName `
     --os-type Windows `
     --name npwin `
-    --node-count 2 `
+    --node-count $winNodes `
     --kubernetes-version $aksLastVersion
